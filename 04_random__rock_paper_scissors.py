bc24255ea15a314d4427a13a21fb58b095fd200b
Michael Primavera <mr.michaelprimavera@outlook.com>
2026-02-18 12:20:19 -0500

    Created an interactive rock-paper-scissors game using the random module.

---
+++ 04_random__rock_paper_scissors.py
@@ -0,0 +1,86 @@
+# import the random library
+import random as rd
+
+# ascii art from https://www.asciiart.eu/
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+# set options to a list
+options = [rock, paper, scissors]
+# select a random choice for the computer
+computers_choice = rd.choice(options)
+
+# create a users choice variable for printing ascii art
+users_choice = ''
+# prompt user for their chioce and save as variable
+users_choice_str = input('Which do You choose, "rock", "paper", or "scissors"?\n')
+
+if users_choice_str not in ['rock', 'paper', 'scissors']:
+    print("You entered an invalid choice. You lose!")
+else:
+    # set variable for winner
+    winner = ''
+
+    # logic to determine winner
+    if users_choice_str == 'rock':
+        users_choice = rock
+        if computers_choice == rock:
+            winner = 'tie'
+        elif computers_choice == paper:
+            winner = 'computer'
+        else:
+            winner = 'You'
+    elif users_choice_str == 'paper':
+        users_choice = paper
+        if computers_choice == rock:
+            winner = 'You'
+        elif computers_choice == paper:
+            winner = 'tie'
+        else:
+            winner = 'computer'
+    else:
+        users_choice = scissors
+        if computers_choice == rock:
+            winner = 'computer'
+        elif computers_choice == paper:
+            winner = 'You'
+        else:
+            winner = 'tie'
+
+    # print the choices
+    print(f"The computer chose\n{computers_choice}")
+    print(f"You chose\n {users_choice}")
+
+    # print the winner
+    if winner != 'tie':
+        if winner == 'You':
+            print(f"{winner} are the winner!")
+        else:
+            print(f"The {winner} is the winner!")
+    else:
+        print(f"The game was a {winner}!")
