8c94c8f0118c908e85a0822ae449b2e263bf3378
Michael Primavera <mr.michaelprimavera@outlook.com>
2026-02-20 15:23:08 -0500

    Created the caesar_cypher file for encrypting and decrypting alphabetic letters by a user-specified shift amount

---
+++ 07_caesar_cypher.py
@@ -0,0 +1,63 @@
+from ascii_art import caesar_cypher_logo
+
+# data--letters in alphabet
+letters = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m',
+    'n','o','p','q','r','s','t','u','v','w','x','y','z'
+]
+
+# print welcome logo
+print("Welcome to:")
+print(caesar_cypher_logo)
+
+
+def cypher():
+    # prompt the user to encrypt or decrypt
+    choice = input("Type 'encode' to encrypt, and 'decode' to decrypt:\n")
+    # attain message and shift from user
+    message_raw = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number\n"))
+    message_encrypted = ''
+
+    # if shift > 25
+
+    # change shift direction if decode is selected
+    if choice == 'decode':
+        shift = 26 - shift
+
+    # account for negative values
+    if shift < 0:
+        while shift < -26:
+            shift += 26
+        shift = shift + 26
+
+
+    # perform the shift and output the letters
+    # get shift for each letter in the message
+    for letter_m in message_raw:
+        # create temporary variables for individual letter shift
+        count = 0
+        shifted_letter = 0
+        if letter_m not in letters:
+            message_encrypted += letter_m
+        else:
+            # count the letters until the match to get the current position of the letter in the alphabet
+            while letter_m != letters[count]:
+                count += 1
+            # add the shift to the current position to get the position of the shifted letter
+            shifted_letter = count + shift
+
+            # subtract 26 if the value is greater than the length of the alphabet to cycle back to the beginning of the alphabet
+            while shifted_letter >= 26:
+                shifted_letter -= 26
+
+            # add the shifted letter to the encrypted message
+            message_encrypted += letters[shifted_letter]
+
+    # print the encrypted message to the user
+    print(f"Here's the encoded result {message_encrypted}")
+    play_again = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if play_again == 'yes':
+        cypher()
+
+cypher()
